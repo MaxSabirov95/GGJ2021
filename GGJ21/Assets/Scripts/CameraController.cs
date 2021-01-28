67ed554f1fd0cd0fb84a59f6c840958ca338ac35
MYoshua64 <MYoshua64@users.noreply.github.com>
2021-01-28 21:55:28 +0200

    Player Controller

---
+++ GGJ21/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float followSpeed = 2f;
+    [SerializeField] private float upOffset = 0.5f;
+    [SerializeField] private float marginOffset = 1f;
+    private Vector3 followPosition;
+
+    //private void Start()
+    //{
+    //    followPosition = transform.position;
+    //}
+    
+    public float marginOffsetX;
+
+    void Start()
+    {
+        marginOffsetX = marginOffset * (float)Screen.width / Screen.height;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Debug.Log(Mathf.Abs(Player.instance.transform.position.x - transform.position.x));
+        if (Mathf.Abs(Player.instance.transform.position.y - transform.position.y - upOffset) > marginOffset ||
+            Mathf.Abs(Player.instance.transform.position.x - transform.position.x) > marginOffsetX)
+        {
+            //Debug.Log("Should be following!");
+            followPosition = Player.instance.transform.position - Vector3.forward * 10f + upOffset * Vector3.up;
+        }
+        transform.position = Vector3.Lerp(transform.position, followPosition, followSpeed * Time.deltaTime);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(marginOffsetX, marginOffset, 1) * 2);
+    }
+}
---
+++ GGJ21/Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+
+    [SerializeField] private float moveSpeed = 2.5f;
+    [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float groundCheckRadius = 0.5f;
+    [SerializeField] private Transform groundCheckPositionTransform;
+
+    private Rigidbody2D playerRB;
+    private Vector2 inputMovement;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        inputMovement = xMovement * Vector2.right * moveSpeed;
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            playerRB.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        HandleJump();
+    }
+
+    private void HandleJump()
+    {
+        if (playerRB.velocity.y < 0)
+        {
+            playerRB.velocity += Physics2D.gravity * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (playerRB.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            playerRB.velocity += Physics2D.gravity * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
+    private void Move()
+    {
+        playerRB.AddForce(inputMovement);
+        Vector2 vel = playerRB.velocity;
+        vel.x = Mathf.Clamp(vel.x, -maxSpeed, maxSpeed);
+        playerRB.velocity = vel;
+    }
+
+    bool IsGrounded()
+    {
+        Collider2D groundCollider = Physics2D.OverlapCircle(groundCheckPositionTransform.position, groundCheckRadius, ground);
+        return groundCollider != null;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(groundCheckPositionTransform.position, groundCheckRadius);
+    }
+}
