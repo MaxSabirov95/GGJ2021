5f4197753bc73e5e375d357258b2fa2b4a4b23ad
MYoshua64 <MYoshua64@users.noreply.github.com>
2021-01-29 01:41:33 +0200

    Fog Fix + Platform Rotator

---
+++ GGJ21/Assets/Scripts/PlatformRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRotator : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(-Vector3.forward * 100 * rotateSpeed * Time.deltaTime);
+    }
+}
