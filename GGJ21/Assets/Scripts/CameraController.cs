41345c96d18b93c4691c98507c17166b30e8cfca
MYoshua64 <MYoshua64@users.noreply.github.com>
2021-01-29 00:27:46 +0200

    Fog Shader

--- GGJ21/Assets/Scripts/CameraController.cs
+++ GGJ21/Assets/Scripts/CameraController.cs
@@ -7,12 +7,9 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float followSpeed = 2f;
     [SerializeField] private float upOffset = 0.5f;
     [SerializeField] private float marginOffset = 1f;
+    [SerializeField] private float lookAheadOffset = 1f;
     private Vector3 followPosition;
-
-    //private void Start()
-    //{
-    //    followPosition = transform.position;
-    //}
+    private float lookAheadActual;
     
     public float marginOffsetX;
 
@@ -28,6 +25,11 @@ public class CameraController : MonoBehaviour
             Mathf.Abs(Player.instance.transform.position.x - transform.position.x) > marginOffsetX)
         {
             followPosition = Player.instance.transform.position - Vector3.forward * 10f + upOffset * Vector3.up;
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > Mathf.Epsilon)
+            {
+                lookAheadActual = lookAheadOffset;
+            }
+            followPosition += lookAheadActual * Input.GetAxisRaw("Horizontal") * Vector3.right;
         }
         transform.position = Vector3.Lerp(transform.position, followPosition, followSpeed * Time.deltaTime);
     }
