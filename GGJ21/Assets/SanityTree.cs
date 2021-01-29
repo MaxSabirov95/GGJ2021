2d972136797bcbbe82770c3c9c1be0d37918a488
MYoshua64 <MYoshua64@users.noreply.github.com>
2021-01-30 01:46:33 +0200

    Sanity Tree

---
+++ GGJ21/Assets/SanityTree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanityTree : MonoBehaviour
+{
+    [SerializeField] private SanityPiece[] pieces;
+    [SerializeField] private Sprite[] stageSprites;
+
+    private SpriteRenderer treeRenderer;
+    private int recoveryStage = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        treeRenderer = GetComponent<SpriteRenderer>();
+        foreach (SanityPiece piece in pieces)
+        {
+            piece.OnPieceRetrieved += RestoreTree;
+        }
+    }
+
+    void RestoreTree()
+    {
+        recoveryStage++;
+        treeRenderer.sprite = stageSprites[recoveryStage - 1];
+    }
+}
