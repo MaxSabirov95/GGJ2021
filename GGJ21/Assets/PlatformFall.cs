3a9e27297fd1f9e7e0de1bd5378e3ed083cb0ce8
MYoshua64 <MYoshua64@users.noreply.github.com>
2021-01-30 00:26:06 +0200

    Level 1 And Level Streaming

--- GGJ21/Assets/PlatformFall.cs
+++ GGJ21/Assets/PlatformFall.cs
@@ -6,17 +6,21 @@ public class PlatformFall : MonoBehaviour
 {
     Rigidbody2D platformRB;
     [SerializeField] float fallMultiplier;
+    [SerializeField] private Transform holdingVine;
 
     private void Start()
     {
         platformRB = GetComponent<Rigidbody2D>();
+        GetComponentInChildren<VinePlatform>().OnVineDestroyed += Fall;
     }
-    void FixedUpdate()
+
+    void Fall()
     {
-        if(VinePlatform.vineDestroyed == true)
-        {
-            platformRB.gravityScale = 1f;
-            platformRB.velocity += Physics2D.gravity * (fallMultiplier - 1) * Time.fixedDeltaTime;
-        }
+        platformRB.isKinematic = false;
+        platformRB.velocity += Physics2D.gravity * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        //if (holdingVine != null)
+        //{
+        //    Destroy(holdingVine.gameObject);
+        //}
     }
 }
