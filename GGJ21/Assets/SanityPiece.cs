71fd4458815feb2141ce096e249d796001f7c61d
MYoshua64 <MYoshua64@users.noreply.github.com>
2021-01-30 01:05:32 +0200

    Sanity Piece

--- GGJ21/Assets/SanityPiece.cs
+++ GGJ21/Assets/SanityPiece.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class SanityPiece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            //play victory sound, fade screen, then teleport back to tree
+        }
     }
 }
