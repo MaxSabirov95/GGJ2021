9e00472d93e3a311139892a8598d7df94f27884b
Aquid16 <aqu2id@gmail.com>
2021-01-29 04:23:27 +0200

    Settings Menu

--- GGJ21/Assets/Scripts/UI/MainMenu.cs
+++ GGJ21/Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void LoadMenu()
+    public AudioMixer audioMixer;
+    public GameObject settingsMenu;
+    public GameObject mainMenu;
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void OpenSettings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+    public void Back()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void LoadLevel()
     {
         SceneManager.LoadScene("Main Level Scene");
         Time.timeScale = 1f;        
