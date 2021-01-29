3d62bb46546ad7be1c3fc2c5cc0a96f9c79dcc78
Aquid16 <aqu2id@gmail.com>
2021-01-29 21:54:58 +0200

    Main Menu UI NEW

--- GGJ21/Assets/Scripts/UI/MainMenu.cs
+++ GGJ21/Assets/Scripts/UI/MainMenu.cs
@@ -7,20 +7,40 @@ public class MainMenu : MonoBehaviour
     public AudioMixer audioMixer;
     public GameObject settingsMenu;
     public GameObject mainMenu;
-    public void SetVolume (float volume)
+    public GameObject creditMenu;
+    public void SetMusicVolume (float music)
     {
-        audioMixer.SetFloat("volume", volume);
+        audioMixer.SetFloat("Music", music);
+    }
+    public void SetSFXVolume(float sfx)
+    {
+        audioMixer.SetFloat("SFX", sfx);
+    }
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        creditMenu.SetActive(false);
     }
 
     public void OpenSettings()
     {
         mainMenu.SetActive(false);
         settingsMenu.SetActive(true);
+        creditMenu.SetActive(false);
+    }
+    public void OpenCredits()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        creditMenu.SetActive(true);
     }
     public void Back()
     {
         settingsMenu.SetActive(false);
         mainMenu.SetActive(true);
+        creditMenu.SetActive(false);
     }
     public void LoadLevel()
     {
