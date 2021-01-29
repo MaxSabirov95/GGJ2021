d69116315ad6b5fb89145233dddcb9a458395782
Max Sabirov <maxim-sabirov@rambler.ru>
2021-01-29 02:37:39 +0200

    Revert "Merge remote-tracking branch 'origin/Almog's-Branch' into Max"
    This reverts commit 2684e7eb8aa1de3512e617615f50ae3bcd47dd2c, reversing
    changes made to 2026fe763b5d1a1bf2d8201e0e9a612fb419dac0.

--- GGJ21/Assets/Scripts/Player.cs
+++ GGJ21/Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public static Player instance;
-
+    
     [SerializeField] private float moveSpeed = 2.5f;
     [SerializeField] private float maxSpeed = 5f;
     [SerializeField] private float jumpForce = 8f;
@@ -15,16 +15,11 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask ground;
     [SerializeField] private float groundCheckRadius = 0.5f;
     [SerializeField] private Transform groundCheckPositionTransform;
-    [SerializeField] private float initialSanity = 100f;
-    [SerializeField] private float sanityDegrationSpeed = 5f;
-    [SerializeField] private PhysicsMaterial2D slipperyMaterial2D;
-    [SerializeField] private PhysicsMaterial2D fullFrictionMaterial2D;
 
     private Rigidbody2D playerRB;
     private Vector2 inputMovement;
-    private Vector2 groundAngleVector;
-    private Collider2D playerCollider;
-    private float currentSanity;
+    private SpriteRenderer sprite;
+    private bool isGhost;
 
     void Awake()
     {
@@ -35,40 +30,27 @@ public class Player : MonoBehaviour
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
-        playerCollider = GetComponent<Collider2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         HandleInput();
-        HandleSlopes();
-    }
-
-    private void HandleSlopes()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, ground);
-        if (hit)
-        {
-            Debug.DrawRay(hit.point, Vector2.Perpendicular(hit.normal), Color.green);
-            groundAngleVector = -Vector2.Perpendicular(hit.normal);
-            float xMovement = Input.GetAxisRaw("Horizontal");
-            if (Mathf.Abs(xMovement) < Mathf.Epsilon)
-            {
-                playerCollider.sharedMaterial = Vector2.Angle(hit.normal, Vector2.up) > 10f ? fullFrictionMaterial2D : slipperyMaterial2D;
-            }
-            else playerCollider.sharedMaterial = slipperyMaterial2D;
-        }
-        else groundAngleVector = Vector2.right;
     }
 
     private void HandleInput()
     {
         float xMovement = Input.GetAxisRaw("Horizontal");
-        inputMovement = xMovement * groundAngleVector * moveSpeed;
+        inputMovement = xMovement * Vector2.right * moveSpeed;
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             playerRB.AddForce(Vector2.up * jumpForce);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            ToggleGhost();
         }
     }
 
@@ -110,8 +92,24 @@ public class Player : MonoBehaviour
         Gizmos.DrawWireSphere(groundCheckPositionTransform.position, groundCheckRadius);
     }
 
-    public float GetSanityLevel()
+    void ToggleGhost()
     {
-        return currentSanity;
+        isGhost = !isGhost;
+        if (isGhost)
+        {
+            Color spriteColor = sprite.color;
+            spriteColor.a = 125;
+            sprite.color = spriteColor;
+            Physics.IgnoreLayerCollision(11, 9,true);
+            Physics.IgnoreLayerCollision(11, 10, false);
+        }
+        else
+        {
+            Color spriteColor = sprite.color;
+            spriteColor.a = 255;
+            sprite.color = spriteColor;
+            Physics.IgnoreLayerCollision(11, 9, false);
+            Physics.IgnoreLayerCollision(11, 10,true);
+        }
     }
 }
